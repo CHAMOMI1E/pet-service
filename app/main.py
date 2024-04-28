aa0c6890b4eb2d0e30e82c8d789623cec5f16a6c
CHAMOMILE <CHAMOM2LES@outlook.com>
2024-04-28 13:07:55 +0300

    add sign up

---
+++ app/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from config import DB_TOKEN
+from app.routers import users
+import databases
+
+# создаем объект database, который будет использоваться для выполнения запросов
+database = databases.Database(DB_TOKEN)
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+app.include_router(users.router)
+
+
+@app.get("/start")
+async def start():
+    return {"hello": "world"}
+
+
+@app.on_event("startup")
+async def startup():
+    # когда приложение запускается устанавливаем соединение с БД
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    # когда приложение останавливается разрываем соединение с БД
+    await database.disconnect()
---
+++ app/utils/users.py
@@ -0,0 +1,85 @@
+import hashlib
+import random
+import string
+from datetime import datetime, timedelta
+from sqlalchemy import select
+
+from db.models.base import async_session
+from db.models.users import Tokens, Users
+from app.schemas import users as user_schema
+
+
+def get_random_string(length=12):
+    """ Генерирует случайную строку, использующуюся как соль """
+    return "".join(random.choice(string.ascii_letters) for _ in range(length))
+
+
+def hash_password(password: str, salt: str = None):
+    """ Хеширует пароль с солью """
+    if salt is None:
+        salt = get_random_string()
+    enc = hashlib.pbkdf2_hmac("sha256", password.encode(), salt.encode(), 100_000)
+    return enc.hex()
+
+
+def validate_password(password: str, hashed_password: str):
+    """ Проверяет, что хеш пароля совпадает с хешем из БД """
+    salt, hashed = hashed_password.split("$")
+    return hash_password(password, salt) == hashed
+
+
+async def get_user_by_email(email: str):
+    """ Возвращает информацию о пользователе """
+    async with async_session() as session:
+        query = await session.execute(select(Users).where(Users.email == email))
+        return query.scalars.first()
+
+
+async def get_user_by_token(token: str):
+    """ Возвращает информацию о владельце указанного токена """
+    async with async_session as session:
+        try:
+            query = await session.execute(
+                select(Tokens).
+                join(Users).
+                where(Tokens.expires > datetime.now(),
+                      Tokens.token == token))
+            return query.scalars.first()
+        except Exception as e:
+            print(e)
+
+
+async def create_user_token(user_id: int):
+    """ Создает токен для пользователя с указанным user_id """
+    async with async_session() as session:
+        try:
+            new_user_token = Tokens(
+                expires=datetime.now() + timedelta(weeks=2),
+                user_id=user_id,
+            )
+            session.add(new_user_token)
+            session.commit()
+            return new_user_token
+        except Exception as e:
+            print(e)
+
+
+async def create_user(user: user_schema.UserCreate):
+    """ Создает нового пользователя в БД """
+    async with async_session() as session:
+        try:
+            salt = get_random_string()
+            hashed_password = hash_password(user.password, salt)
+            user_id = Users(
+                email=user.email,
+                name=user.name,
+                hashed_password=f"{salt}${hashed_password}"
+            )
+            session.add(user_id)
+            await session.commit()
+            token = await create_user_token(user_id.id)
+            token_dict = {"token": token["token"], "expires": token["expires"]}
+        except Exception as e:
+            print(e)
+
+    return {**user.dict(), "id": user_id, "is_active": True, "token": token_dict}
